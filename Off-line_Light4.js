bd333bb8188900748edb548c40b5a791162ed6e4
ThinkerStain <83018662+ThinkerStain@users.noreply.github.com>
2021-05-07 15:28:39 +0300

    Update Off-line_Light4.js

--- Off-line_Light4.js
+++ Off-line_Light4.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Light4
 // @namespace    http://tampermonkey.net/
-// @version      0.081
+// @version      0.084
 // @description  Упрощаем работу глазам
 // @author       Yuriy.Klimovich@south.rt.ru
 // @include        *argus.south.rt.ru/argus/views/supportservice/incident/*
@@ -9,6 +9,7 @@
 // @include        *onyma/main/clsrv.htms*
 // @include        *onyma/main/dog.htms?*
 // @require      https://github.com/ThinkerStain/UserScript/raw/main/Light4.js
+// @updateURL    https://github.com/ThinkerStain/UserScript/raw/main/Light4.js
 
 
 // ==/UserScript==
