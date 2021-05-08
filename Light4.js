0e8240bb91d4241306f8896c94f92a27fd72e258
ThinkerStain <83018662+ThinkerStain@users.noreply.github.com>
2021-05-08 10:33:33 +0300

    Update Light4.js

--- Light4.js
+++ Light4.js
@@ -8,7 +8,8 @@
 // @include        *argus.south.rt.ru/argus*
 // @include        *onyma/main/clsrv.htms*
 // @include        *onyma/main/dog.htms?*
-
+// @require      https://github.com/ThinkerStain/UserScript/raw/main/Light4.js
+// @updateURL   https://github.com/ThinkerStain/UserScript/raw/main/Light4.js
 
 
 // @unsafeWindow
